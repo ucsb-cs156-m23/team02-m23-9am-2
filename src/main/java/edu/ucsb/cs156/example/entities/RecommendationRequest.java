d1b2b5344cc4f46f3e441e1f6c0a847baeac65f3
Shiyang/Bill <shiyangxin@ucsb.edu>
2023-07-27 14:42:46 -0700

    Add files via upload

---
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -0,0 +1,47 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+////
+
+import java.time.LocalDateTime;
+
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.GeneratedValue;
+////
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "recommendationrequest")
+public class RecommendationRequest {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String code;
+  private String name;
+  private boolean hasSackMeal;
+  private boolean hasTakeOutMeal;
+  private boolean hasDiningCam;
+  private Double latitude;
+  private Double longitude;
+  private long id;
+  private String quarterYYYYQ;
+  private String name;  
+  private LocalDateTime localDateTime;
+}
+////////////////////////////////////////////
+
+
+
+
+
+
+  
+  
