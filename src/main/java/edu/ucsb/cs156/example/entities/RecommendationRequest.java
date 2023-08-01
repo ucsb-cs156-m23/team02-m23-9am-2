95ea7669a35f0ca371e50d22db04cedecf15ce2e
Shiyang/Bill <shiyangxin@ucsb.edu>
2023-07-31 21:17:25 -0700

    Add files via upload

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -23,27 +23,16 @@ import javax.persistence.GeneratedValue;
 @Entity(name = "recommendationrequest")
 public class RecommendationRequest {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private String code;
-  private boolean hasSackMeal;
-  private boolean hasTakeOutMeal;
-  private boolean hasDiningCam;
-  private Double latitude;
-  private Double longitude;
-  private long id;
-  private String quarterYYYYQ;
-  private String name;  
-  private LocalDateTime localDateTime;
+  
+  private String requesterEmai;
+  private String professorEmail;
+  private String explanation;
+  private LocalDateTime setdateRequested;
+  private LocalDateTime setdateNeeded;
+  private boolean done;
+
 }
 ////////////////////////////////////////////
-
-
-
-
-
-
-  
-  
 
 
 
